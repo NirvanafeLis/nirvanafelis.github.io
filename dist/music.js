2cc4a081050a890d6e46d33b52276ec021cb7214
NirvanafeLis <posttruth4u@gmail.com>
2023-11-02 13:35:36 +0800

    Site updated: 2023-11-02 13:35:14

---
+++ dist/music.js
@@ -0,0 +1,11 @@
+const ap = new APlayer({
+    container: document.getElementById('player'),
+    fixed: true,
+    autoplay: true,
+    audio: [{
+        name: 'Soul Kisses',
+        artist: 'Gregg Karukas',
+        url: 'https://music.163.com/song/media/outer/url?id=17989055.mp3',
+        cover: 'http://p1.music.126.net/9uhoOvXPKk3Su1IRQEKZ7g==/109951163358202549.jpg',
+    }]
+});
